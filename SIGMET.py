58734c7abaa7a1ccb8f0194aae76ea989b38f16b
banan1442 <banan1442@outlook.com>
2026-02-25 13:34:45 +0100

    Update SIGMET.py

--- SIGMET.py
+++ SIGMET.py
@@ -2,7 +2,7 @@ import requests
 import json
 from datetime import datetime
 
-FIR_LIST = ["UUWV", "ESAA", "LTBB", "UNNT", "UNKL", "UIII"]  # список FIR
+FIR_LIST = ["UNNT", "UNKL", "UIII"]  # список FIR
 URL = "https://aviationweather.gov/api/data/isigmet?format=json"
 
 def fetch_sigmet_multi(fir_list):
@@ -71,6 +71,7 @@ for fir_id, data in sigmet_data.items():
     print(data["lat_line"])
     print(f"{fir_id} lon:")
     print(data["lon_line"])
+
 
 
 
